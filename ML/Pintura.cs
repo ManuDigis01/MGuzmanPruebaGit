58ac9fb5b2aa41105cdc2b2e58d5a5300a64c5a7
Leonardo Blancas Uribe <leonardobu28@outlook.com>
2026-01-14 15:34:57 -0600

    Referencias y CapaML Correctamente

---
+++ ML/Pintura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Pintura
+    {
+        public int IdPintura { get; set; }
+        public string Color { get; set; }
+        public string FechaDeProduccion { get; set; }
+        public string FechaDeCaducacion { get; set; }
+    }
+}
